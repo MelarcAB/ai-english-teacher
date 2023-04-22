4561a69991661d030bae32e64a53f77b7e58c330
MelarcAB <melarc.ab@gmail.com>
2023-04-22 12:23:15 +0200

    arreglos visuales + info a config

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -25,14 +25,6 @@
             <form action="{{ route('configuration.store') }}" method="POST">
                 @csrf
                 <div class="mb-4">
-                    <label for="openai_token" class="block text-teal-700">Token de OpenAI:</label>
-                    <input type="text" name="openai_token" id="openai_token" value="{{ auth()->user()->openai_token }}"
-                        class="border rounded px-3 py-2 w-full">
-                    @error('openai_token')
-                        <span class="text-red-500 text-sm">{{ $message }}</span>
-                    @enderror
-                </div>
-                <div class="mb-4">
                     <label for="openai_model" class="block text-teal-700">Modelo de OpenAI:</label>
                     <select name="openai_model" id="openai_model" class="border rounded px-3 py-2 w-full">
                         <option value="gpt-3.5-turbo-0301" @if (auth()->user()->openai_model == 'gpt-3.5-turbo-0301') selected @endif>GPT 3.5 Turbo
@@ -43,7 +35,27 @@
                     @error('openai_model')
                         <span class="text-red-500 text-sm">{{ $message }}</span>
                     @enderror
+
                 </div>
+                <div class="mb-4">
+                    <label for="openai_token" class="block text-teal-700">Token de OpenAI:</label>
+                    <input type="text" name="openai_token" id="openai_token" value="{{ auth()->user()->openai_token }}"
+                        class="border rounded px-3 py-2 w-full">
+                    @error('openai_token')
+                        <span class="text-red-500 text-sm">{{ $message }}</span>
+                    @enderror
+                    <div class=" m-2 p-4 bg-gray-100 rounded-md shadow-md text-sm">
+                        <ol class="list-decimal list-inside">
+                            <li>Inicia sesión en tu cuenta de OpenAI. <a class="text-teal-600 hover:text-teal-700"
+                                    target="_blank" href="https://beta.openai.com/">Accede desde
+                                    aquí</a></li>
+                            <li>Accede a la sección de API Keys.</li>
+                            <li>Crea y copia tu token de la API de OpenAI.</li>
+                            <li>Pega el token en el campo.</li>
+                        </ol>
+                    </div>
+                </div>
+
 
                 <div class="flex justify-end">
                     <button type="submit"
